b43e381191b4d0e128cc1a86abdc363603e90313
bzerangue <brian.zerangue@gmail.com>
2014-03-14 18:24:18 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).foundation();
 
 $( document ).ready(function() {
   var current = new Date();
-  var regstarts  = new Date("March 15, 2014 00:00:00")
+  var regstarts  = new Date("March 14, 2014 00:00:00")
   var regends = new Date("March 28, 2014 12:00:00")
 
   if(current.getTime()<regends.getTime() && current.getTime()>regstarts.getTime()){
